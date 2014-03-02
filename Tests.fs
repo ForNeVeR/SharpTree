90ae56eb1f79f4a41ee5866a5156c21e066945be
ForNeVeR <neverthness@gmail.com>
2014-03-02 22:02:37 +0700

    Tests: rewrite tests. Closes #1 abd #2.

---
+++ Tests.fs
@@ -0,0 +1,15 @@
+﻿namespace Sharp.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Splay.Tree
+
+[<TestClass>]
+type SharpTreeTest() =
+    [<TestMethod>]
+    member this.mapTest() =
+        let map = Map.ofList <| [for x in [1..10] -> x, x.ToString()]
+        let tree = ref empty<int, string>
+        Map.iter (fun k v -> tree := insert k v !tree) map
+        Map.iter (fun k v -> let v', tree' = find k !tree
+                             tree := tree'
+                             Assert.AreEqual(Some v, v')) map
